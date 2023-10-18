88ac44ffdcebada54a8556beef06184bf2783bf1
Денис <d-e-n-i-s-ne@yandex.ru>
2023-10-18 23:45:42 +0300

    переместил контроллеры напрямую в папку controllers, починил updateUser (нашел ошибку), добавил в папку postman обновленную коллекцию, перенес маппинг в контроллер

--- src/main/java/ru/rosatom/documentflow/controllers/UserController.java
+++ src/main/java/ru/rosatom/documentflow/controllers/UserController.java
@@ -1,4 +1,4 @@
-package ru.rosatom.documentflow.controllers.admin;
+package ru.rosatom.documentflow.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,21 +9,32 @@ import org.springframework.web.bind.annotation.*;
 import ru.rosatom.documentflow.dto.UserCreateDto;
 import ru.rosatom.documentflow.dto.UserReplyDto;
 import ru.rosatom.documentflow.dto.UserUpdateDto;
+import ru.rosatom.documentflow.mappers.UserMapper;
+import ru.rosatom.documentflow.mappers.UserPassportMapper;
+import ru.rosatom.documentflow.models.User;
+import ru.rosatom.documentflow.models.UserOrganization;
+import ru.rosatom.documentflow.models.UserPassport;
+import ru.rosatom.documentflow.services.UserOrganizationService;
 import ru.rosatom.documentflow.services.UserService;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/admin/users")
-public class UserControllerAdmin {
+public class UserController {
 
-    private final UserService service;
+    private final UserService userService;
+    private final UserOrganizationService organizationService;
+    private final UserPassportMapper passportMapper;
+    private final UserMapper userMapper;
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -31,7 +42,11 @@ public class UserControllerAdmin {
 
         log.info("Received a request to create a user " + dto);
 
-        return service.createUser(dto);
+        UserOrganization organization = organizationService.getOrganization(dto.getOrganizationId());
+        UserPassport passport = passportMapper.dtoToObject(dto);
+        User user = userMapper.dtoToObject(dto, organization, passport);
+
+        return userMapper.objectToReplyDto(userService.createUser(organization, passport, user));
     }
 
     @PatchMapping("/{userId}")
@@ -40,26 +55,34 @@ public class UserControllerAdmin {
 
         log.info("Received a request to update a user {}. userId = {}", dto, userId);
 
-        return service.updateUser(dto, userId);
+        return userMapper.objectToReplyDto(userService.updateUser(dto, userId));
     }
 
     @PutMapping("/password/{userId}")
     public ResponseEntity<?> setUserPassword(@Valid @Size(min = 8, message = "password is too short") @RequestParam(required = true, value = "password") String password,
                                              @PathVariable Long userId) {
         log.info("Received a request to set password to user with userId = {}", userId);
-        if (service.setPasswordToUser(password, userId)) {
+        if (userService.setPasswordToUser(password, userId)) {
             return ResponseEntity.ok("Password has been set to user with id " + userId);
         } else {
             return new ResponseEntity<>("User with id " + userId + " not found", HttpStatus.BAD_REQUEST);
         }
     }
 
-    @GetMapping("/id/{userId}")
-    public UserReplyDto getUserDto(@PathVariable Long userId) {
+    @GetMapping("/{userId}")
+    public UserReplyDto getUser(@PathVariable Long userId) {
 
         log.info("A request was received to search for a user with an id {}", userId);
 
-        return service.getUserDto(userId);
+        return userMapper.objectToReplyDto(userService.getUser(userId));
+    }
+
+    @GetMapping
+    public List<UserReplyDto> getAllUsers() {
+
+        log.info("A search request was received for all users");
+
+        return userService.getAllUsers().stream().map(userMapper::objectToReplyDto).collect(Collectors.toList());
     }
 
     @GetMapping("/ids")
@@ -70,7 +93,9 @@ public class UserControllerAdmin {
 
         log.info("Received a request to search for all users for params: ids {}, sort {}, from {}, size {}", ids, sort, from, size);
 
-        return service.getUsers(ids, sort.toUpperCase(), from, size);
+        return userService.getUsers(ids, sort.toUpperCase(), from, size).stream()
+                .map(userMapper::objectToReplyDto)
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/phone/{phone}")
@@ -78,7 +103,7 @@ public class UserControllerAdmin {
 
         log.info("Received a request to search user for telephone {}", phone);
 
-        return service.getUserByPhone(phone);
+        return userMapper.objectToReplyDto(userService.getUserByPhone(phone));
     }
 
     @GetMapping("/email/{email}")
@@ -86,7 +111,7 @@ public class UserControllerAdmin {
 
         log.info("Received a request to search user for email {}", email);
 
-        return service.getUserByEmail(email);
+        return userMapper.objectToReplyDto(userService.getUserByEmail(email));
     }
 
     @GetMapping("/passport/{passport}")
@@ -94,7 +119,7 @@ public class UserControllerAdmin {
 
         log.info("Received a request to search user for passport {}", passport);
 
-        return service.getUserByPassport(passport);
+        return userMapper.objectToReplyDto(userService.getUserByPassport(passport));
     }
 
     @DeleteMapping("/{userId}")
@@ -103,6 +128,6 @@ public class UserControllerAdmin {
 
         log.info("Received a request to delete a user with an id " + userId);
 
-        service.deleteUser(userId);
+        userService.deleteUser(userId);
     }
 }
