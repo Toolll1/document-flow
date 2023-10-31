e3c78e7a29fe0bea544240e271c2be3fca18e7ef
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-10-31 11:46:10 +0300

    Format fix

--- src/main/java/ru/rosatom/documentflow/exceptions/IllegalProcessStatusException.java
+++ src/main/java/ru/rosatom/documentflow/exceptions/IllegalProcessStatusException.java
@@ -5,6 +5,6 @@ import ru.rosatom.documentflow.models.DocProcessStatus;
 
 public class IllegalProcessStatusException extends BadRequestException {
     public IllegalProcessStatusException(DocProcess docProcess, DocProcessStatus attemptStatus) {
-        super("Нельзя перевести процесс со статусом %s в статус %s".formatted(docProcess.getStatus(), attemptStatus));
+        super(String.format("Нельзя перевести процесс со статусом %s в статус %s", docProcess.getStatus(), attemptStatus));
     }
 }
