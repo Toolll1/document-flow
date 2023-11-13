38444d19b9ec0ff2fa6b9373b8fb156cf69357d5
vchumachenko <v.chumachenko@yahoo.com>
2023-11-13 20:40:26 +0300

    Добавлена разметка DocAttributeDto и DocTypeDto

--- src/main/java/ru/rosatom/documentflow/dto/DocAttributeDto.java
+++ src/main/java/ru/rosatom/documentflow/dto/DocAttributeDto.java
@@ -1,10 +1,15 @@
 package ru.rosatom.documentflow.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Атрибут документа")
 public class DocAttributeDto {
+    @Schema(description = "ID атрибута")
     private Long id;
+    @Schema(description = "Наименование атрибута")
     private String name;
+    @Schema(description = "Тип атрибута")
     private String type;
 }
