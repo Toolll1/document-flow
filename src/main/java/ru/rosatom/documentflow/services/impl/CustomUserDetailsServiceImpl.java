62d2fc346beff8a97b42ceff1a08a940d629b5c1
HukoJlauII <polosatay_zebra@mail.ru>
2023-11-26 17:34:24 +0300

    500 error code while auth fixed

--- src/main/java/ru/rosatom/documentflow/services/impl/CustomUserDetailsServiceImpl.java
+++ src/main/java/ru/rosatom/documentflow/services/impl/CustomUserDetailsServiceImpl.java
@@ -5,13 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.rosatom.documentflow.repositories.UserRepository;
 import ru.rosatom.documentflow.services.CustomUserDetailsService;
 
 @Slf4j
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
     private final UserRepository userRepository;
