0f13d6e24534ae62d68bc8f25bb9981468bac511
HukoJlauII <polosatay_zebra@mail.ru>
2023-11-27 11:42:08 +0300

    Code duplicates removed

--- src/main/java/ru/rosatom/documentflow/services/impl/EmailServiceImpl.java
+++ src/main/java/ru/rosatom/documentflow/services/impl/EmailServiceImpl.java
@@ -1,37 +1,34 @@
 package ru.rosatom.documentflow.services.impl;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.text.StringSubstitutor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import ru.rosatom.documentflow.models.DocProcess;
-import ru.rosatom.documentflow.models.MessagePattern;
-import ru.rosatom.documentflow.models.MessageTemplate;
-import ru.rosatom.documentflow.services.EmailService;
 import ru.rosatom.documentflow.services.MessageTemplateService;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.util.Map;
 
 @Service
 @Slf4j
 @Profile("prod")
-@RequiredArgsConstructor
-public class EmailServiceImpl implements EmailService {
-
-    @Value("${spring.mail.username}")
-    private String from;
+public class EmailServiceImpl extends AbstractEmailService {
 
     private final String MESSAGE_ENCODING = "UTF-8";
 
+    @Bean
+    public JavaMailSenderImpl javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+
     private final JavaMailSenderImpl javaMailSender;
 
-    private final MessageTemplateService messageTemplateService;
+    public EmailServiceImpl(MessageTemplateService messageTemplateService, JavaMailSenderImpl javaMailSender) {
+        super(messageTemplateService);
+        this.javaMailSender = javaMailSender;
+    }
 
 
     @Override
@@ -48,28 +45,6 @@ public class EmailServiceImpl implements EmailService {
             log.warn("Unable to send message: ", e);
         }
 
-    }
-
-    @Override
-    public void sendDocProcessAgreementMessageForRecipient(DocProcess docProcess) {
-        MessageTemplate messageTemplate = messageTemplateService.getMessageTemplateByPattern(MessagePattern.AGREEMENT);
-        Map<String, String> messageParams = Map.of(
-                "firstName", docProcess.getRecipient().getFirstName(),
-                "documentName", docProcess.getDocument().getName(),
-                "mail", docProcess.getSender().getEmail(),
-                "fullName", docProcess.getSender().getLastName() + ' ' + docProcess.getSender().getFirstName() + ' ' + docProcess.getSender().getPatronymic());
-        StringSubstitutor substitutor = new StringSubstitutor(messageParams);
-        sendDocProcessMessage(docProcess.getRecipient().getEmail(), substitutor.replace(messageTemplate.getBody()), messageTemplate.getSubject());
-    }
-
-    @Override
-    public void sendDocProcessResultMessageForOwner(DocProcess docProcess, MessagePattern messagePattern) {
-        MessageTemplate messageTemplate = messageTemplateService.getMessageTemplateByPattern(messagePattern);
-        Map<String, String> messageParams = Map.of(
-                "firstName", docProcess.getSender().getFirstName(),
-                "documentName", docProcess.getDocument().getName());
-        StringSubstitutor substitutor = new StringSubstitutor(messageParams);
-        sendDocProcessMessage(docProcess.getSender().getEmail(), substitutor.replace(messageTemplate.getBody()), messageTemplate.getSubject());
     }
 
 
--- src/main/java/ru/rosatom/documentflow/services/impl/EmailServiceStub.java
+++ src/main/java/ru/rosatom/documentflow/services/impl/EmailServiceStub.java
@@ -1,30 +1,20 @@
 package ru.rosatom.documentflow.services.impl;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.text.StringSubstitutor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-import ru.rosatom.documentflow.models.DocProcess;
-import ru.rosatom.documentflow.models.MessagePattern;
-import ru.rosatom.documentflow.models.MessageTemplate;
-import ru.rosatom.documentflow.services.EmailService;
 import ru.rosatom.documentflow.services.MessageTemplateService;
-
-import java.util.Map;
 
 @Service
 @Slf4j
 @Profile("dev")
-@RequiredArgsConstructor
-public class EmailServiceStub implements EmailService {
+public class EmailServiceStub extends AbstractEmailService {
 
-    @Value("${spring.mail.username}")
-    private String from;
 
-    private final MessageTemplateService messageTemplateService;
-
+    public EmailServiceStub(MessageTemplateService messageTemplateService) {
+        super(messageTemplateService);
+    }
 
     @Override
     public void sendDocProcessMessage(String to, String text, String subject) {
@@ -33,28 +23,6 @@ public class EmailServiceStub implements EmailService {
                 "To: {}\n" +
                 "Subject: {}\n" +
                 "Body: {}\n", from, to, subject, text);
-    }
-
-    @Override
-    public void sendDocProcessAgreementMessageForRecipient(DocProcess docProcess) {
-        MessageTemplate messageTemplate = messageTemplateService.getMessageTemplateByPattern(MessagePattern.AGREEMENT);
-        Map<String, String> messageParams = Map.of(
-                "firstName", docProcess.getRecipient().getFirstName(),
-                "documentName", docProcess.getDocument().getName(),
-                "mail", docProcess.getSender().getEmail(),
-                "fullName", docProcess.getSender().getLastName() + ' ' + docProcess.getSender().getFirstName() + ' ' + docProcess.getSender().getPatronymic());
-        StringSubstitutor substitutor = new StringSubstitutor(messageParams);
-        sendDocProcessMessage(docProcess.getRecipient().getEmail(), substitutor.replace(messageTemplate.getBody()), messageTemplate.getSubject());
-    }
-
-    @Override
-    public void sendDocProcessResultMessageForOwner(DocProcess docProcess, MessagePattern messagePattern) {
-        MessageTemplate messageTemplate = messageTemplateService.getMessageTemplateByPattern(messagePattern);
-        Map<String, String> messageParams = Map.of(
-                "firstName", docProcess.getSender().getFirstName(),
-                "documentName", docProcess.getDocument().getName());
-        StringSubstitutor substitutor = new StringSubstitutor(messageParams);
-        sendDocProcessMessage(docProcess.getSender().getEmail(), substitutor.replace(messageTemplate.getBody()), messageTemplate.getSubject());
     }
 
 
