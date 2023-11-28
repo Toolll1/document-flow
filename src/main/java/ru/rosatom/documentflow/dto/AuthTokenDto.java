351197e4533ad4cbae9a67beaf772feddd6e6037
Andrew97422 <nosoff.4ndr@yandex.ru>
2023-11-28 20:35:56 +0300

    Изменён возврат юзера при логине

---
+++ src/main/java/ru/rosatom/documentflow/dto/AuthTokenDto.java
@@ -0,0 +1,61 @@
+package ru.rosatom.documentflow.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@AllArgsConstructor
+@Builder
+@RequiredArgsConstructor
+@Data
+@NoArgsConstructor(force = true)
+public class AuthTokenDto {
+    private String token;
+
+    @Schema(description = "Фамилия", minLength = 2, maxLength = 100)
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private final String lastName;
+
+    @Schema(description = "Имя", minLength = 2, maxLength = 100)
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private final String firstName;
+
+    @Schema(description = "Отчество", minLength = 2, maxLength = 100)
+    @Size(min = 2, max = 100)
+    private final String patronymic;
+
+    @Schema(description = "Дата рождения", minLength = 10, maxLength = 10)
+    @NotBlank
+    @Size(min = 10, max = 10)
+    private final String dateOfBirth;
+
+    @Schema(description = "Email", minLength = 6, maxLength = 320)
+    @Email
+    @NotBlank
+    @Size(min = 6, max = 320)
+    private final String email;
+
+    @Schema(description = "Номер телефона", minLength = 11, maxLength = 11)
+    @NotBlank
+    @Size(min = 11, max = 11)
+    private final String phone;
+
+    @Schema(description = "ID организации")
+    @NotNull
+    private final Long organizationId;
+
+    @Schema(description = "Роль пользователя")
+    @NotNull
+    private final String role;
+
+    @Schema(description = "Описание пользователя", minLength = 1, maxLength = 320)
+    @NotBlank
+    @Size(min = 1, max = 320)
+    private String post;
+}
--- src/main/java/ru/rosatom/documentflow/services/impl/AuthServiceImpl.java
+++ src/main/java/ru/rosatom/documentflow/services/impl/AuthServiceImpl.java
@@ -2,6 +2,7 @@ package ru.rosatom.documentflow.services.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +12,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.rosatom.documentflow.configuration.JWT.JWTUtil;
+import ru.rosatom.documentflow.dto.AuthTokenDto;
+import ru.rosatom.documentflow.repositories.UserRepository;
 import ru.rosatom.documentflow.services.AuthService;
 
 
@@ -24,12 +27,24 @@ public class AuthServiceImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
 
+    private final UserRepository userRepository;
+
+    private final ModelMapper mapper;
+
 
     @Override
     public ResponseEntity<?> loginUser(String email, String password) {
         final Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(email, password));
         SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = jwtUtil.generateToken(email);
+
+        var user = userRepository.findByEmail(email).orElseThrow();
+        AuthTokenDto authToken = new AuthTokenDto();
+        mapper.map(user, authToken);
+        authToken.setToken(token);
+
         return new ResponseEntity<>(jwtUtil.generateToken(email), HttpStatus.ACCEPTED);
     }
 
