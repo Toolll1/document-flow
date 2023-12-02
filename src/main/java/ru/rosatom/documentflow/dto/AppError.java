2d6c8bd0cfbb8c4fa5d4e7ff87438a6330384208
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-12-02 23:28:59 +0300

    Token object fix

--- src/main/java/ru/rosatom/documentflow/dto/AppError.java
+++ src/main/java/ru/rosatom/documentflow/dto/AppError.java
@@ -1,11 +1,13 @@
 package ru.rosatom.documentflow.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 @Schema(description = "Описание ошибки")
 public class AppError {
   @Schema(description = "Текст ошибки")
