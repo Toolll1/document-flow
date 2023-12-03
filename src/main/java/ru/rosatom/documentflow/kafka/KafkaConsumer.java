86eaf58a08f22e01f87705de51b97be11a4b7489
SavaStyle <sava.90@inbox.ru>
2023-12-03 18:14:29 +0300

    Kafka

---
+++ src/main/java/ru/rosatom/documentflow/kafka/KafkaConsumer.java
@@ -0,0 +1,15 @@
+/*
+
+package ru.rosatom.documentflow.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "documentFinalStatus", groupId = "idOne")
+    public void listenGroupIdOne(String message) {
+        System.out.println("Получено сообщение в группе idOne: " + message);
+    }
+}*/
