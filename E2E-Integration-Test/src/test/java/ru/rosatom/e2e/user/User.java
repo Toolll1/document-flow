50c2618e07c49270cf97eddb460e6204d7d60055
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-12-09 01:59:00 +0300

    Added E2E tests

---
+++ E2E-Integration-Test/src/test/java/ru/rosatom/e2e/user/User.java
@@ -0,0 +1,19 @@
+package ru.rosatom.e2e.user;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class User{
+    private Integer id;
+    private String lastName;
+    private String firstName;
+    private String patronymic;
+    private String dateOfBirth;
+    private String email;
+    private String phone;
+    private Integer organizationId;
+    private UserRole role;
+    private String post;
+}
---
+++ E2E-Integration-Test/src/test/java/ru/rosatom/e2e/userTests/A_Authorization/UserAuthorizationTests.java
@@ -0,0 +1,85 @@
+package ru.rosatom.e2e.userTests.A_Authorization;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import ru.rosatom.e2e.BasicHttpTest;
+import ru.rosatom.e2e.Environment;
+import ru.rosatom.e2e.application.AppError;
+import ru.rosatom.e2e.user.User;
+import ru.rosatom.e2e.user.UserAuthorizationRequest;
+import ru.rosatom.e2e.user.UserAuthorizationResponse;
+import ru.rosatom.e2e.user.UserEndpoints;
+
+import java.util.stream.Stream;
+
+@DisplayName("User authorization tests")
+public class UserAuthorizationTests extends BasicHttpTest {
+
+
+    @DisplayName("Incorrect authorization: wrong password")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "\"'@!:',m)(*&^"})
+    public void testAuthWithWrongPassword(String password) {
+        testAuthWithIncorrectParams(faker.internet().emailAddress(), password);
+    }
+
+    @DisplayName("Incorrect authorization: wrong email")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "hello", "@", "\"'@!:',m)(*&^"})
+    public void testAuthWithWrongEmail(String email) {
+        testAuthWithIncorrectParams(email, faker.internet().password());
+    }
+
+
+    @Test
+    @DisplayName("Success authorization")
+    public void testSuccessLogin(){
+        UserAuthorizationResponse userAuthorizationResponse = testSuccessAuth(new UserFedotovAuthRequest());
+        setContextValue(Environment.USER_FEDOTOV_AUTHORIZATION, userAuthorizationResponse);
+    }
+
+
+
+
+
+
+    private void testAuthWithIncorrectParams(String email, String password) {
+        sendAuthRequest(new UserAuthorizationRequest(email, password))
+                .expectStatus().isUnauthorized()
+                .expectBody(AppError.class)
+                .value(response -> {
+                    assert response != null;
+                    assert response.getMessage() != null;
+                });
+    }
+
+    private UserAuthorizationResponse testSuccessAuth(UserAuthorizationRequest userAuthorizationRequest){
+        return sendAuthRequest(userAuthorizationRequest)
+                .expectStatus().isAccepted()
+                .expectBody(UserAuthorizationResponse.class)
+                .value(response -> {
+                    assert response != null;
+                    assert response.getToken() != null;
+                    assert response.getUser() != null;
+                    User user = response.getUser();
+                    Stream.of(user.getId(), user.getLastName(), user.getFirstName(), user.getPatronymic(),
+                            user.getDateOfBirth(), user.getEmail(), user.getPhone(), user.getOrganizationId(),
+                            user.getRole(), user.getPost()).forEach(Assertions::assertNotNull);
+                })
+                .returnResult().getResponseBody();
+    }
+
+
+
+    private WebTestClient.ResponseSpec sendAuthRequest(UserAuthorizationRequest userAuthorizationRequest) {
+        return client.post().uri(UserEndpoints.AUTHORIZATION.getUrl())
+                .bodyValue(userAuthorizationRequest)
+                .exchange();
+
+
+    }
+}
