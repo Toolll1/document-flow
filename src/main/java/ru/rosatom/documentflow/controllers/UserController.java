feaacb34f38eb3e07e7d0682731be422599fe5aa
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2024-01-03 15:38:27 +0300

    fixed bad answer when password is null

--- src/main/java/ru/rosatom/documentflow/controllers/UserController.java
+++ src/main/java/ru/rosatom/documentflow/controllers/UserController.java
@@ -12,21 +12,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.rosatom.documentflow.dto.PasswordDto;
 import ru.rosatom.documentflow.dto.UserCreateDto;
 import ru.rosatom.documentflow.dto.UserReplyDto;
@@ -91,16 +81,12 @@ public class UserController {
             "(@userServiceImpl.isAllowed(#userId, #user) && hasAuthority('COMPANY_ADMIN'))")
     @PostMapping("/password/{userId}")
     @SecurityRequirement(name = "JWT")
-    public ResponseEntity<?> setUserPassword(
+    public void setUserPassword(
             @RequestBody @Valid @Parameter (description = "Пароль пользователя") PasswordDto passwordDto,
             @PathVariable @Parameter(description = "ID пользователя") Long userId,
             @AuthenticationPrincipal @Parameter(description = "Пользователь", hidden = true) User user) {
         log.info("Received a request to set password to user with userId = {}", userId);
-        if (userService.setPasswordToUser(passwordDto.getPassword(), userId)) {
-            return new ResponseEntity<>(HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("User with id " + userId + " not found", HttpStatus.BAD_REQUEST);
-        }
+        userService.setPasswordToUser(passwordDto.getPassword(), userId);
     }
 
     @Operation(summary = "Получить пользователя по ID")
