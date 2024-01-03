cabfa5753d77deb3410392c05baddfe3e8bc6949
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2024-01-03 15:43:49 +0300

    Changed 409 to 400 bad request when validation error

--- src/main/java/ru/rosatom/documentflow/exceptions/ErrorHandler.java
+++ src/main/java/ru/rosatom/documentflow/exceptions/ErrorHandler.java
@@ -52,14 +52,14 @@ public class ErrorHandler {
         return createAppError(e, HttpStatus.BAD_REQUEST);
     }
 
-  
+    @ExceptionHandler
     public ResponseEntity<AppError> handleDateTimeParseException(final DateTimeParseException e) {
         return createAppError(e, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
     public ResponseEntity<ValidationError> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
-        return createValidationError(e, HttpStatus.CONFLICT);
+        return createValidationError(e, HttpStatus.BAD_REQUEST);
     }
 
 
