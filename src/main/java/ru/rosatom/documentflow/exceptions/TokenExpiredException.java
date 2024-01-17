2c5e404bb4b6ffb3e3c6366aa5bdfa78641eeb33
Vasily Chumachenko <89158036+vchumachenko@users.noreply.github.com>
2024-01-17 18:07:52 +0300

    Revert "hotfix/403-for-expired-token"

--- src/main/java/ru/rosatom/documentflow/exceptions/TokenExpiredException.java
+++
@@ -1,9 +0,0 @@
-package ru.rosatom.documentflow.exceptions;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class TokenExpiredException  extends AuthenticationException {
-    public TokenExpiredException(String message) {
-        super(message);
-    }
-}
